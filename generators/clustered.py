c1b500d3ba79dd8ef11fe6d93c09e71d9dfd3167
Mert <mertkosan@gmail.com>
2019-03-08 13:40:36 -0800

    minor changes

--- generators/clustered.py
+++ generators/clustered.py
@@ -47,9 +47,9 @@ class ClusteredDynamicGraph(GraphGen):
         if not self._is_chain:
             self._n += int(np.round(np.random.exponential(self._increase_n)))
             if self._p > self._threshold:
-                self._p += np.random.normal(0, 0.001)
+                self._p += np.random.normal(0, 0.005)
             else:
-                self._p += np.random.normal(0.001, 0.001)
+                self._p += np.random.normal(0.005, 0.005)
         # TODO : elif self.chain...
 
     def _construct_graphs(self, graph_count):
